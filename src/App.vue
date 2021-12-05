d985a395b9bc27dbbcb5e01dfac6f1c42e4a302e
Naoto Maeda <naoto.maeda.d3@mhi.com>
2021-12-05 15:47:24 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button @click="loadFile">問題ファイルをロード</button>
+
+  <p>問題</p>
+  <h1>{{word}}</h1>
+  <p>答え</p>
+  <h1 v-if="isShown">{{answer}}</h1>
+
+  <button v-if="!isShown" @click="showAnswer">答えを確認</button>
+  <button v-if="isShown" @click="moveNext">次へ</button>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {ref} from 'vue'
+import yaml from 'js-yaml'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+  setup() {
+    let wordList=[]
+    const word = ref('')
+    const answer = ref('')
+    const isShown = ref(false)
+    const loadFile = async () => {
+      let FileSystemHandles
+      [FileSystemHandles] = await window.showOpenFilePicker();
+      let file = await FileSystemHandles.getFile()
+      let text = await file.text()
+      wordList = yaml.load(text)
+      word.value = wordList.pop()
+    }
+    const moveNext = async () => {
+      word.value = wordList.pop()
+      isShown.value = false     
+
+      const response = await fetch(`https://script.google.com/macros/s/AKfycbzZtvOvf14TaMdRIYzocRcf3mktzGgXvlFvyczo/exec?text=${word.value}&source=en&target=ja`)
+      const data = await response.json()
+      answer.value = data.text  
+    }
+    const showAnswer = () => {
+      isShown.value = true
+    }
+    return {word,isShown,loadFile,moveNext,showAnswer,answer}
   }
 }
 </script>
