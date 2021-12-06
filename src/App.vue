4f960cd22f68a4c45b3d36be37566b816d48990b
Naoto Maeda <naoto.maeda.d3@mhi.com>
2021-12-06 17:31:37 +0900

    changed to <imput> for file select so smart phone can operte

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
 <template>
 
   <template v-if="phase==='TITLE'">
-    <h1>問題くん</h1>
+    <h1 class="title">問題くん</h1>
+    <!--
     <img class="button" src=".\assets\drive_folder_upload_black_24dp.svg" @click="loadFile">
+    -->
+    <label for="file-selector"><img class="button" src=".\assets\drive_folder_upload_black_24dp.svg" ></label>
+    <input id="file-selector" type="file" accept="*.yaml" @change="fileSelect">
   </template>
 
   <template v-else-if="phase==='QUESTION' || phase==='ANSWER'">
-    <p>問題</p>
+    <h2>問題</h2>
     <h1>{{word}}</h1>
 
-    <p>答え</p>
+    <h2>答え</h2>
     <h1 v-if="phase==='ANSWER'">{{answer}}</h1>
     <h1 v-else>???</h1>
 
   <img class="button" src="./assets\skip_next_black_24dp.svg" v-if="phase==='QUESTION'" @click="showAnswer">
+  <div class="thumbs">
   <img class="button" src=".\assets\thumb_up_black_24dp.svg" v-if="phase==='ANSWER'" @click="moveNext">
   <img class="button" src=".\assets\thumb_down_black_24dp.svg" v-if="phase==='ANSWER'" @click="moveNext">
+  </div>
   </template>
 
   <template v-else>
@@ -34,37 +40,55 @@ export default {
     const word = ref('')
     const answer = ref('')
     const phase = ref('TITLE')
-    const loadFile = async () => {
-      let FileSystemHandles
-      [FileSystemHandles] = await window.showOpenFilePicker();
-      let file = await FileSystemHandles.getFile()
-      let text = await file.text()
+//    const loadFile = async () => {
+      //let FileSystemHandles
+      //[FileSystemHandles] = await window.showOpenFilePicker();
+      //let file = await FileSystemHandles.getFile()
+      //let text = await file.text()
+      //wordList = yaml.load(text)
+      //moveNext()
+    //}
+    const fileSelect = async () => {
+      const input = document.getElementById('file-selector')
+      const file = input.files[0]
+      const text = await file.text()
       wordList = yaml.load(text)
       moveNext()
     }
     const moveNext = async () => {
       word.value = wordList.pop()
-      if (word.value) {
-        phase.value = 'QUESTION'
-      } else {
-        phase.value = 'END'
-      }
 
       let u = new SpeechSynthesisUtterance();
       u.lang = 'en-US';
       u.text = word.value? word.value : 'Good Job!';
       speechSynthesis.speak(u);
 
+      if (word.value) {
+        phase.value = 'QUESTION'
+      } else {
+        phase.value = 'END'
+        return
+      }
+
       const response = await fetch(`https://script.google.com/macros/s/AKfycbzZtvOvf14TaMdRIYzocRcf3mktzGgXvlFvyczo/exec?text=${word.value}&source=en&target=ja`)
       const data = await response.json()
       answer.value = data.text  
+
+      await sleep(1)
+
+      showAnswer()
     }
     const showAnswer = () => {
       phase.value = 'ANSWER'
     }
-    return {word,phase,loadFile,moveNext,showAnswer,answer}
+    return {word,phase,fileSelect,moveNext,showAnswer,answer}
   }
 }
+
+function sleep(sec) {
+  return new Promise(resolve => setTimeout(resolve, sec*1000));
+}
+    
 </script>
 
 <style>
@@ -75,8 +99,22 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
 }
 .button {
   width: 200px;
+}
+#file-selector {
+  display: none;
+}
+.title {
+  font-size: 4rem;
+}
+.thumbs {
+  display: flex;
+}
+div.thumbs img {
+  flex: 1;
 }
 </style>
